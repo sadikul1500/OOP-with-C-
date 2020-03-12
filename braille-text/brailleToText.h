a84518de2915489bf54ccf8775085a237e332565
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-03-13 04:38:28 +0600

    Add files via upload

---
+++ braille-text/brailleToText.h
@@ -0,0 +1,25 @@
+#ifndef brailleToText_H
+#define brailleToText_H
+#include<iostream>
+#include<fstream>
+#include<unordered_map>
+
+using namespace std;
+
+class brailleToText
+{
+    private:
+        string* fileList;
+    public:
+        BrailleToText(String *fileList)
+        {
+            this.fileList = fileList;
+        }
+
+        string getText()
+
+
+
+
+};
+#endif // brailleToText_H
