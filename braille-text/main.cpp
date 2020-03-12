a5c39010b7ff7dc234dd613c6428aa1686d9e23e
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-03-13 04:41:20 +0600

    Add files via upload

--- braille-text/main.cpp
+++ braille-text/main.cpp
@@ -1,19 +1,19 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//    cout << "Hello world!" << endl;
-//    return 0;
-//}
 #include<iostream>
-#include "commonSymbols.h"
+#include<fstream>
+#include<filesystem>
+#include "brailleToText.h"
+#include "brailleToEnglish.h"
+#include "brailleToBangla.h"
+//#include "fileManager.h"
+#include "postTextProcess.h"
 
 using namespace std;
 
 int main()
 {
-    CommonSymbols cs;
-    cout<<cs.getPunctuation()["000001011011"]<<endl;
+
+    string path = "/path/to/brailleDoc";
+    for (const auto & entry : fs::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
+
 }
---
+++ braille-text/test.cpp
@@ -0,0 +1,81 @@
+////#include<iostream>
+////#include "commonSymbols.h"
+////
+////using namespace std;
+////
+////int main()
+////{
+////    CommonSymbols cs;
+////    cout<<cs.getPunctuation()["000001011011"]<<endl;
+////}
+//#include<iostream>
+//#include<iterator>
+//
+//using namespace std;
+//
+//void print(string* arr)
+//{
+//    for (int i = 0; i < 4; i++)
+//      cout << arr[i] << endl;
+//}
+//
+//int main() {
+//   string animals[] = {"Elephant", "Lion", "Deer", "Tiger", "Monkey"}; //The
+//   //string type array
+//   cout<<sizeof(animals) / sizeof(*animals)<<endl;
+//   print(animals);
+//
+//}
+
+
+// CPP code to check if a key is present
+// in an unordered_map
+#include <bits/stdc++.h>
+#include<boost/filesystem.hpp>
+using namespace std;
+using namespace boost::filesystem;
+
+// Function to check if the key is present or not
+string check_key(unordered_map<string, string> m, string key)
+{
+	// Key is not present
+	if (m.find(key) != m.end())
+		return "Present";
+
+	return "Not Present";
+}
+
+// Driver
+int main()
+{
+    path p("F:/braille");
+    for (auto i = directory_iterator(p); i != directory_iterator(); i++)
+    {
+        if (!is_directory(i->path())) //we eliminate directories
+        {
+            cout << i->path().filename().string() << endl;
+        }
+        else
+            continue;
+    }
+}
+
+
+#include <boost/filesystem.hpp>
+using namespace std;
+using namespace boost::filesystem;
+
+int main()
+{
+    path p("D:/AnyFolder");
+    for (auto i = directory_iterator(p); i != directory_iterator(); i++)
+    {
+        if (!is_directory(i->path())) //we eliminate directories
+        {
+            cout << i->path().filename().string() << endl;
+        }
+        else
+            continue;
+    }
+}
+
