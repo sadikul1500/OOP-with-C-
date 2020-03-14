499020c4971b28e72e974d07136afb984fc377f3
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-03-14 19:12:19 +0600

    Add files via upload

---
+++ braille-text/splitText.h
@@ -0,0 +1,25 @@
+#ifndef splitText_H
+#define splitText_H
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class SplitText
+{
+    private:
+
+    public:
+        vector<string> split(const string& input, const string& regx)
+        {
+            regex re(regx);
+            sregex_token_iteration first{input.begin(), input.end(), re, -1}, last;
+
+            return {first, last};
+        }
+
+
+};
+
+
+
+#endif // splitText_H
