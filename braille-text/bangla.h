c87f02a0bc7f89e893d5cd17a4ad855ced65dd88
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-03-16 12:08:53 +0600

    Update bangla.h

--- braille-text/bangla.h
+++ braille-text/bangla.h
@@ -1,3 +1,5 @@
+#ifndef Bangla_H
+#define Bangla_H
 #include<iostream>
 #include<unordered_map>
 #include "commonSymbols.h"
@@ -15,21 +17,21 @@ class Bangla: public CommonSymbols
             {"উ", "ু"}, {"ঊ", "ূ"},
             {"এ", "ে"}, {"ঐ", "ৈ"},
             {"ও", "ো"}, {"ঔ", "ৌ"}, {"ঋ", "ৃ"}
-        }
-        # print(symbolToKar)
+        };
+        //# print(symbolToKar)
 
         unordered_map<string, string> vol_spe = {
-            {"010100", "ই",}
-            {"101001", "উ",}
+            {"010100", "ই"},
+            {"101001", "উ"},
             {"101010", "ও"}
-        }
+        };
 
         unordered_map<string, string> volume = {
             {"100000", "অ"}, {"001110", "আ"},
             {"001010", "ঈ"}, {"110011", "ঊ"},
             {"000010111010", "ঋ"}, {"100010", "এ"},
             {"001100", "ঐ"}, {"010101", "ঔ"}
-        }
+        };
 
         unordered_map<string, string> numbers = {
             {"010110", "০"}, {"100000", "১"},
@@ -37,7 +39,7 @@ class Bangla: public CommonSymbols
             {"100110", "৪"}, {"100010", "৫"},
             {"110100", "৬"}, {"110110", "৭"},
             {"110010", "৮"}, {"010100", "৯"}
-        }
+        };
 
         unordered_map<string, string> consonant = {
             {"101000", "ক"}, {"101101", "খ"}, {"110110", "গ"}, {"110001", "ঘ"}, {"001101", "ঙ"},
@@ -49,11 +51,11 @@ class Bangla: public CommonSymbols
             {"100101", "শ"}, {"011100", "স"}, {"111101", "ষ"}, {"110010", "হ"}, {"111110", "ক্ষ"},
             {"100011", "জ্ঞ"}, {"110111", "ড়"}, {"111011", "ঢ়"}, {"010001", "য়"}, {"010000011110", "ৎ"},
             {"000100", "্‌"}, {"000011", "ং"}, {"000001", "ঃ"}, {"001000", "ঁ"}
-        }
+        };
 
         unordered_map<string, string> fourLetters = {
             {"কষময", "ক্ষ্ম্য"}, {"নতরয", "ন্ত্র্য"}
-        }
+        };
 
     public:
 
@@ -63,34 +65,34 @@ class Bangla: public CommonSymbols
             setBanglaDictionary();
         }
 
-        unordered_map<string, string> getFourLetters(self)
+        unordered_map<string, string> getFourLetters()
         {
             return fourLetters;
         }
 
-        unordered_map<string, string> getConsonant(self)
+        unordered_map<string, string> getConsonant()
         {
             return consonant;
         }
 
-        unordered_map<string, string> getNumbers(self)
+        unordered_map<string, string> getNumbers()
         {
             return numbers;
         }
 
 
-        unordered_map<string, string> getVolume(self)
+        unordered_map<string, string> getVolume()
         {
             return volume;
         }
 
-        unordered_map<string, string> getVol_spe(self)
+        unordered_map<string, string> getVol_spe()
         {
             return vol_spe;
         }
 
 
-        unordered_map<string, string>getSymbolToKar(self)
+        unordered_map<string, string>getSymbolToKar()
         {
             return symbolToKar;
         }
@@ -99,10 +101,10 @@ class Bangla: public CommonSymbols
         {
             banglaDictionary.insert(vol_spe.begin(), vol_spe.end());
             banglaDictionary.insert(volume.begin(), volume.end());
-            banglaDictionary.insert(punctuation.begin(), punctuation.end());
+            banglaDictionary.insert(getPunctuation().begin(), getPunctuation().end());
             banglaDictionary.insert(consonant.begin(), consonant.end());
-            banglaDictionary.insert(consonant.begin(), consonant.end());
-            banglaDictionary.insert(hosonto.begin(), hosonto.end());
+            banglaDictionary.insert(getDot().begin(), getDot().end());
+            banglaDictionary.insert(getHosonto().begin(), getHosonto().end());
 
         }
 
@@ -113,3 +115,4 @@ class Bangla: public CommonSymbols
 
 
 };
+#endif // Bangla_H
