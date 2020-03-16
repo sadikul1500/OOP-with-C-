21445381845c8701b88700fba163140b63930b88
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-03-16 12:11:38 +0600

    Update brailleToText.h

--- braille-text/brailleToText.h
+++ braille-text/brailleToText.h
@@ -2,21 +2,39 @@
 #define brailleToText_H
 #include<iostream>
 #include<fstream>
-#include<unordered_map>
+#include<bits/stdc++.h>
+#include "splitText.h"
+//#include<unordered_map>
 
 using namespace std;
 
-class brailleToText
+class BrailleToText
 {
-    private:
-        string* fileList;
+    //private:
+    //    string* fileList;
     public:
-        BrailleToText(String *fileList)
-        {
-            this.fileList = fileList;
-        }
+        virtual string getBrailleToText(vector<string> text);
 
-        string getText()
+        string getText(string file, BrailleToText *brailleToText)
+        {
+            ifstream iFile(file);
+            string line;
+            SplitText splitText;
+            string text = "";
+
+            while(getline(iFile, line))
+            {
+                vector<string> word = splitText.split(line, " space ");
+                for(int i=0; i<word.size(); i++)
+                {
+                    vector<string> letters = splitText.split(word[i], " ");
+                    text += brailleToText->getBrailleToText(letters);
+                }
+                text += "\n";
+            }
+            iFile.close();
+            return text;
+        }
 
 
 
